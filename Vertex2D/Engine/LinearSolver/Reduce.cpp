3b1586e9e6cf77c5982fd0b2bfbd3994ed859ba8
Maximilian Maldacker <mmaldacker@gmail.com>
2016-04-14 14:05:22 +0200

    improved reduce operator

--- Vertex2D/Engine/LinearSolver/Reduce.cpp
+++ Vertex2D/Engine/LinearSolver/Reduce.cpp
@@ -11,44 +11,22 @@
 namespace Fluid
 {
 
-const char * ReduceVert = GLSL(
-   in vec2 a_Position;
-   in vec2 a_TexCoords;
-   out vec2 v_texCoord;
-
-   uniform mat4 u_Projection;
-   uniform sampler2D u_texture;
-
-   const vec2 off = vec2(0.5);
-
-   void main()
-   {
-       gl_Position = u_Projection * vec4(a_Position, 0.0, 1.0);
-
-       vec2 h = textureSize(u_texture, 0);
-       vec2 k = ceil(h/vec2(2.0));
-
-       v_texCoord = (vec2(2.0) * (a_TexCoords * k - off) + off) / h;
-   }
-);
-
 const char * ReduceFrag = GLSL(
-   in vec2 v_texCoord;
    out vec4 colour_out;
    
    uniform sampler2D u_texture;
    
-   const vec4 q = vec4(1.0);
-   
    void main()
    {
-       vec4 p;
-       p.x = texture(u_texture, v_texCoord).x;
-       p.y = textureOffset(u_texture, v_texCoord, ivec2(0,1)).x;
-       p.z = textureOffset(u_texture, v_texCoord, ivec2(1,0)).x;
-       p.w = textureOffset(u_texture, v_texCoord, ivec2(1,1)).x;
-       
-       colour_out = vec4(dot(p,q), 0.0, 0.0, 0.0);
+       ivec2 pos = 2 * ivec2(gl_FragCoord.xy - 0.5);
+
+       float x = 0.0;
+       x += texelFetch(u_texture, pos + ivec2(0,0), 0).x;
+       x += texelFetch(u_texture, pos + ivec2(1,0), 0).x;
+       x += texelFetch(u_texture, pos + ivec2(0,1), 0).x;
+       x += texelFetch(u_texture, pos + ivec2(1,1), 0).x;
+
+       colour_out = vec4(x, 0.0, 0.0, 0.0);
    }
 );
 
@@ -69,7 +47,7 @@ const char * MultiplyFrag = GLSL(
 );
 
 Reduce::Reduce(glm::vec2 size)
-    : reduce(ReduceVert, ReduceFrag)
+    : reduce(Renderer::Shader::PositionVert, ReduceFrag)
     , multiply(Renderer::Shader::TexturePositionVert, MultiplyFrag)
 {
     while(size.x > 1.0f && size.y > 1.0f)
