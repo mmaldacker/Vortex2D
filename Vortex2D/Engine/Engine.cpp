c430a16f7156c55deac9e3ae633cbea47c9c0f80
Maximilian Maldacker <mmaldacker@gmail.com>
2016-06-18 23:41:23 +0200

    fix projection step

--- Vortex2D/Engine/Engine.cpp
+++ Vortex2D/Engine/Engine.cpp
@@ -61,37 +61,31 @@ const char * ProjectFrag = GLSL(
     {
         vec2 cell = texture(u_texture, v_texCoord).xy;
 
-        float pxp = textureOffset(u_pressure, v_texCoord, ivec2(1,0)).x;
+        float p = texture(u_pressure, v_texCoord).x;
         float pxn = textureOffset(u_pressure, v_texCoord, ivec2(-1,0)).x;
-        float pyp = textureOffset(u_pressure, v_texCoord, ivec2(0,1)).x;
         float pyn = textureOffset(u_pressure, v_texCoord, ivec2(0,-1)).x;
 
-        float cxp = textureOffset(u_obstacles, v_texCoord, ivec2(2,0)).x;
-        float cxn = textureOffset(u_obstacles, v_texCoord, ivec2(-2,0)).x;
-        float cyp = textureOffset(u_obstacles, v_texCoord, ivec2(0,2)).x;
-        float cyn = textureOffset(u_obstacles, v_texCoord, ivec2(0,-2)).x;
-
-        vec2 pGrad = vec2(pxp-pxn, pyp-pyn);
+        vec2 pGrad = vec2(p-pxn, p-pyn);
 
         vec2 mask = vec2(1.0);
         vec2 obsV = vec2(0.0);
 
-        if (cxp > 0.0)
+        if (textureOffset(u_obstacles, v_texCoord, ivec2(2,0)).x > 0.0)
         {
             mask.x = 0.0;
             obsV.x = textureOffset(u_obstacles_velocity, v_texCoord, ivec2(1,0)).x;
         }
-        if (cxn > 0.0)
+        if (textureOffset(u_obstacles, v_texCoord, ivec2(-2,0)).x > 0.0)
         {
             mask.x = 0.0;
             obsV.x = textureOffset(u_obstacles_velocity, v_texCoord, ivec2(-1,0)).x;
         }
-        if (cyp > 0.0)
+        if (textureOffset(u_obstacles, v_texCoord, ivec2(0,2)).x > 0.0)
         {
             mask.y = 0.0;
             obsV.y = textureOffset(u_obstacles_velocity, v_texCoord, ivec2(0,1)).y;
         }
-        if (cyn > 0.0)
+        if (textureOffset(u_obstacles, v_texCoord, ivec2(0,-2)).x > 0.0)
         {
             mask.y = 0.0;
             obsV.y = textureOffset(u_obstacles_velocity, v_texCoord, ivec2(0,-1)).y;
