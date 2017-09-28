4266fad0544f117aa156b555bcf88c8ba0627433
Maximilian Maldacker <mmaldacker@gmail.com>
2017-09-28 16:45:21 +0200

    test showing particles getting x velocity (when they shouldnt)

--- Tests/Engine/WorldTests.cpp
+++ Tests/Engine/WorldTests.cpp
@@ -14,14 +14,34 @@ using namespace Vortex2D::Fluid;
 
 extern Device* device;
 
+void PrintParticles(const glm::ivec2& size, World& world)
+{
+    Buffer particles(*device, vk::BufferUsageFlagBits::eStorageBuffer, true, 8*size.x*size.y*sizeof(Particle));
+    std::vector<Particle> particleData(8*size.x*size.y);
+    
+    ExecuteCommand(*device, [&](vk::CommandBuffer commandBuffer)
+    {
+       particles.CopyFrom(commandBuffer, world.Particles());
+    });
+
+    int particleCount = world.Count().GetCount();
+    particles.CopyTo(particleData);
+    for (int i = 0; i < particleCount; i++)
+    {
+        std::cout //<< "(" << particleData[i].Position.x << "," << particleData[i].Position.y << ")"
+                  << "(" << particleData[i].Velocity.x << "," << particleData[i].Velocity.y << ")";
+    }
+    std::cout << std::endl;
+}    
+
 TEST(WorldTests, Velocity)
 {
     Dimensions size(glm::ivec2(20), 1.0f);
     Vortex2D::Fluid::World world(*device, size, 0.01f);
 
     // Add particles
-    Vortex2D::Renderer::IntRectangle fluidArea(*device, glm::vec2(5.0f), glm::ivec4(4));
-    fluidArea.Position = {5.0f, 5.0f};
+    Vortex2D::Renderer::IntRectangle fluidArea(*device, glm::vec2(1.0f), glm::ivec4(4));
+    fluidArea.Position = {10.0f, 2.0f};
 
     fluidArea.Initialize(world.Count());
     fluidArea.Update(world.Count().Orth, size.InvScale);
@@ -33,23 +53,8 @@ TEST(WorldTests, Velocity)
     world.Count().Submit();
     device->Handle().waitIdle();
 
-    // Draw obstacle
-    Vortex2D::Renderer::Rectangle obstacle(*device, {10.0f, 5.0f}, glm::vec4(-1.0f));
-    obstacle.Position = {5.0f, 7.0f};
-    obstacle.Initialize(world.SolidPhi());
-    obstacle.Update(world.SolidPhi().Orth, size.InvScale);
-    world.SolidPhi().Record([&](vk::CommandBuffer commandBuffer)
-    {
-        Vortex2D::Renderer::Clear(size.Size.x, size.Size.y, {-1.0f, 0.0f, 0.0f, 0.0f}).Draw(commandBuffer);
-        obstacle.Draw(commandBuffer, world.SolidPhi());
-    });
-    world.SolidPhi().Submit();
-
-    world.SolidPhi().Reinitialise();
-    device->Handle().waitIdle();
-
     // Draw gravity
-    Vortex2D::Renderer::Rectangle gravity(*device, size.Size, {0.0f, -0.5f, 0.0f, 0.0f});
+    Vortex2D::Renderer::Rectangle gravity(*device, size.Size, {0.0f, -0.01f, 0.0f, 0.0f});
 
     gravity.Initialize(world.Velocity());
     gravity.Update(world.Velocity().Orth, {});
@@ -58,36 +63,14 @@ TEST(WorldTests, Velocity)
     {
         gravity.Draw(commandBuffer, world.Velocity());
     });
-    world.Velocity().Submit();
 
     // Step
-    world.SolveDynamic();
-    device->Handle().waitIdle();
-
-    // Verify
-    Texture output(*device, size.Size.x, size.Size.y, vk::Format::eR32G32Sfloat, true);
-    ExecuteCommand(*device, [&](vk::CommandBuffer commandBuffer)
+    for (int i = 0; i < 10; i++)
     {
-        output.CopyFrom(commandBuffer, world.Velocity());
-    });
-
-    PrintVelocity(size.Size, output);
-
-    Buffer particles(*device, vk::BufferUsageFlagBits::eStorageBuffer, true, 8*size.Size.x*size.Size.y*sizeof(Particle));
-    std::vector<Particle> particleData(8*size.Size.x*size.Size.y);
-    ExecuteCommand(*device, [&](vk::CommandBuffer commandBuffer)
-    {
-       particles.CopyFrom(commandBuffer, world.Particles());
-    });
-
-    int particleCount = world.Count().GetCount();
-    std::cout << "Particle Count: " << particleCount << std::endl;
-
-    particles.CopyTo(particleData);
-    for (int i = 0; i < particleCount; i++)
-    {
-        std::cout << "(" << particleData[i].Position.x << "," << particleData[i].Position.y << ")"
-                  << "(" << particleData[i].Velocity.x << "," << particleData[i].Velocity.y << ")"
-                  << std::endl;
+      world.Velocity().Submit();    
+      world.SolveDynamic();
+      device->Handle().waitIdle();
+      
+      PrintParticles(size.Size, world);
     }
 }
