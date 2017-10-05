bb831a91ad05a0c525933cc2db26fb32c762c3a8
Max Maldacker <mmaldacker@gmail.com>
2017-10-05 18:30:47 +0200

    rename script

--- Scripts/Breakdown.py
+++ Scripts/Breakdown.py
@@ -2,18 +2,18 @@ import argparse
 import json
 import pygal
 
-parser = argparse.ArgumentParser(description='CG Breakdown')
+parser = argparse.ArgumentParser(description='Breakdown')
 parser.add_argument('file', metavar='file', type=str,
                    help='breakdown file')
 
 args = parser.parse_args()
 
 breakdown_chart = pygal.Pie()
-breakdown_chart.title = 'CG breakdown'
+breakdown_chart.title = 'breakdown'
 
 file = open(args.file, 'r')
 for line in file.readlines():
     split_line = line.split(":")
     breakdown_chart.add(split_line[0], float(split_line[1].strip()))
 
-breakdown_chart.render_to_file("cg_breakdown.svg")
+breakdown_chart.render_to_file("breakdown.svg")
