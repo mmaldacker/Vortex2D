21b06f24b901446004b23738ff60977a0b5447e9
Maximilian Maldacker <mmaldacker@gmail.com>
2017-12-22 15:12:11 +0100

    test with dedicated memory allocation

--- Vortex2D/Renderer/Buffer.cpp
+++ Vortex2D/Renderer/Buffer.cpp
@@ -46,6 +46,7 @@ GenericBuffer::GenericBuffer(const Device& device,
     VkBufferCreateInfo vkBufferInfo = bufferInfo;
     VmaAllocationCreateInfo allocInfo = {};
     allocInfo.usage = memoryUsage;
+    allocInfo.flags = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT;
     if (vmaCreateBuffer(device.Allocator(),
                         &vkBufferInfo,
                         &allocInfo,
