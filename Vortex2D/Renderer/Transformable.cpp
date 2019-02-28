ed361a63cf4dc3d0ca8325029b8c102570a78a21
Maximilian Maldacker <mmaldacker@gmail.com>
2019-02-28 22:30:09 +0100

    clean up transformable update function

--- Vortex2D/Renderer/Transformable.cpp
+++ Vortex2D/Renderer/Transformable.cpp
@@ -22,10 +22,10 @@ Transformable::Transformable()
 
 void Transformable::Update()
 {
-    mTransform = glm::translate(glm::vec3{(glm::vec2)Position, 0.0f});
-    mTransform = glm::scale(mTransform, glm::vec3{(glm::vec2)Scale, 1.0f});
-    mTransform = glm::rotate(mTransform, glm::radians((float)Rotation), glm::vec3{0.0f, 0.0f, 1.0f});
-    mTransform = glm::translate(mTransform, glm::vec3{-(glm::vec2)Anchor, 0.0f});
+    mTransform = glm::translate(glm::vec3{Position, 0.0f});
+    mTransform = glm::scale(mTransform, glm::vec3{Scale, 1.0f});
+    mTransform = glm::rotate(mTransform, glm::radians(Rotation), glm::vec3{0.0f, 0.0f, 1.0f});
+    mTransform = glm::translate(mTransform, glm::vec3{-Anchor, 0.0f});
 }
 
 const glm::mat4 & Transformable::GetTransform() const
