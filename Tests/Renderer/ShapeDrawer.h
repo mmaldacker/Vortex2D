3c50ac56ca70ac525f9dfef2e69858a668d2044a
Maximilian Maldacker <mmaldacker@gmail.com>
2021-05-24 13:32:23 +0200

    added mesh class

--- Tests/Renderer/ShapeDrawer.h
+++ Tests/Renderer/ShapeDrawer.h
@@ -39,3 +39,28 @@ void DrawSquare(int width,
     }
   }
 }
+
+template <typename T>
+void DrawSquareContour(int width,
+                       int /*height*/,
+                       std::vector<T>& data,
+                       const glm::vec2& centre,
+                       const glm::vec2& size,
+                       T value)
+{
+  for (int i = 1; i <= size.x; i++)
+  {
+    int x = static_cast<int>(i + centre.x);
+    int y = static_cast<int>(centre.y);
+    data[x + y * width] = value;
+    data[x + (y + size.y) * width] = value;
+  }
+
+  for (int j = 1; j <= size.y; j++)
+  {
+    int x = static_cast<int>(centre.x);
+    int y = static_cast<int>(j + centre.y);
+    data[x + y * width] = value;
+    data[x + size.x + y * width] = value;
+  }
+}
